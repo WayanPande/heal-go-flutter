ae7b9f655513ff32284dabcb7202194bb9575adc
WayanPande <yande554@gmail.com>
2022-07-01 15:20:57 +0800

    Added home screen

---
+++ lib/screens/discover_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DiscoverScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Container(
+          child: Text("DISCOVER"),
+        )
+
+    );
+  }
+}
